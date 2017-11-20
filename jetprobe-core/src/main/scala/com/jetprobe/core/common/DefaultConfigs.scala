4106dd25b86a74a1a390a5807f663d9472123b63
Shad <shad4truth@gmail.com>
2017-11-21 01:20:52 +0530

    Fixes #3 - Remove third party css

--- jetprobe-core/src/main/scala/com/jetprobe/core/common/DefaultConfigs.scala
+++ jetprobe-core/src/main/scala/com/jetprobe/core/common/DefaultConfigs.scala
@@ -8,12 +8,13 @@ object DefaultConfigs {
   val cssPathAttr = "report.cssPaths"
   val jsPathAttr = "report.jsPaths"
   val htmlReportAttr = "report.outputPath"
-  val defaultCssPath = "/static/css/materialize.min.css,/static/css/style.css"
+  val defaultCssPath = "/static/css/style.css"
   val defaultJsPaths = "/static/js/progressbar.min.js,/static/js/init.js"
+  val materialCss = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"
   val jqPath = "https://code.jquery.com/jquery-2.2.4.min.js"
   val materializeJs = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"
 
-  def staticResourceConfig(appRoot: String) = Map(cssPathAttr -> defaultCssPath.split(",").map(appRoot + _).mkString(","),
+  def staticResourceConfig(appRoot: String) = Map(cssPathAttr -> (materialCss + "," +defaultCssPath.split(",").map(appRoot + _).mkString(",")),
     jsPathAttr -> (jqPath + "," + materializeJs +","+ (defaultJsPaths.split(",").map(appRoot + _).mkString(","))))
 
 }
