290f663ec11493d4410904168aaf04581068bcc7
Shad <shad4truth@gmail.com>
2018-01-14 21:27:55 +0530

    HDFS Support

--- jetprobe-connectors/jetprobe-hadoop/src/main/scala/com/jetprobe/hadoop/storage/HDFSStorage.scala
+++ jetprobe-connectors/jetprobe-hadoop/src/main/scala/com/jetprobe/hadoop/storage/HDFSStorage.scala
@@ -1,26 +1,29 @@
-package com.jetprobe.hadoop.sinks
+package com.jetprobe.hadoop.storage
 
 import java.io.File
 import java.net.URI
 
 import com.jetprobe.core.action.builder.ActionBuilder
-import com.jetprobe.core.storage.DataSink
+import com.jetprobe.core.parser.{Expr, ExpressionParser}
+import com.jetprobe.core.storage.{Storage, StorageQuery}
+import com.jetprobe.core.structure.Config
+import com.jetprobe.core.validations.ValidationRule
 import com.jetprobe.hadoop.actions._
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.fs.{ContentSummary, FileSystem, Path}
 
 /**
   * @author Shad.
   */
 
-class HDFSSink(val conf: Configuration, val hadoopUserName: String, val fs: FileSystem) extends DataSink {
+class HDFSStorage private[jetprobe](val conf: Configuration, val hadoopUserName: String, val fs: FileSystem) extends Storage {
 
-  def getFileSystem : FileSystem = {
+  def getFileSystem: FileSystem = {
     init(s"/usesr/${hadoopUserName}")
     fs
   }
 
-  private[this] def init(path : String) = {
+  private[this] def init(path: String) = {
     conf.get("fs.defaultFS")
     fs.initialize(new URI(conf.get("fs.defaultFS") + path), conf)
   }
@@ -30,7 +33,7 @@ class HDFSSink(val conf: Configuration, val hadoopUserName: String, val fs: File
   def write(records: Iterator[String], at: String): Unit = {
     init(at)
     val outputStream = fs.create(new Path(at))
-    records.foreach(s => outputStream.writeBytes(s))
+    records.foreach(s => outputStream.writeBytes(s + "\n"))
     outputStream.close()
 
   }
@@ -61,40 +64,65 @@ class HDFSSink(val conf: Configuration, val hadoopUserName: String, val fs: File
     * @param path
     */
   def rmr(path: String): Boolean = {
-    init(path)
-    fs.delete(new Path(path), true)
-  }
-
-  def moveFromLocal(localSrc : String,destination : String) : Unit = {
-    init(destination)
-    fs.copyFromLocalFile(true,new Path(localSrc),new Path(destination))
-  }
-
-  def copyFromLocal(localSrc : String,destination : String) : Unit = {
-    init(destination)
-    fs.copyFromLocalFile(false,new Path(localSrc),new Path(destination))
+    usingFS(fs => fs.delete(new Path(path), true)) match {
+      case Some(x) => x
+      case None => false
+    }
   }
 
 
 
-  def copy(localPath: String, hdfsDir: String): ActionBuilder = new HDFSActionBuilder(conf, new CopyToHDFS(new File(localPath), new Path(hdfsDir)), hadoopUserName)
+  def moveFromLocal(localSrc: String, destination: String): Unit = {
+    usingFS(fs => fs.copyFromLocalFile(true, new Path(localSrc), new Path(destination)))
+  }
 
-  def delete(path: String): ActionBuilder = new HDFSActionBuilder(conf, new DeletePath(new Path(path)), hadoopUserName)
+  def copyFromLocal(localSrc: String, destination: String): Unit = {
+    usingFS(fs => fs.copyFromLocalFile(false, new Path(localSrc), new Path(destination)))
+  }
 
-  //def mkdir(path: String): ActionBuilder = new HDFSActionBuilder(conf, new MkDir(new Path(path)),hadoopUserName)
+  def getFileStatus(path : String) : Option[ContentSummary] = {
+    usingFS { fs =>
+      fs.getContentSummary(new Path(path))
+    }
+  }
 
-  def download(from: String, to: String): ActionBuilder = new HDFSActionBuilder(conf, new DownloadFile(new Path(from), new File(to)), hadoopUserName)
+  def copyToLocal(src : String,destination : String) : Unit = {
+    usingFS(fs => fs.copyToLocalFile(new Path(src),new Path(destination)))
+  }
+
+
+
+ def usingFS[T](fn : FileSystem => T) : Option[T] = {
+
+    val fs = FileSystem.get(conf)
+    try {
+      Some(fn(fs))
+    } catch {
+      case e : Exception =>
+        logger.error(e.getMessage)
+        None
+    }
+    finally fs.close()
+
+  }
 
 }
 
-object HDFSSink {
+class HDFSConfig(uri: String, loginAs: String) extends Config[HDFSStorage] {
 
-  def apply(uri: String, loginAs: String = "root"): HDFSSink = {
-    val conf = new Configuration()
-    conf.set("fs.defaultFS", uri)
-    conf.setBoolean("dfs.client.use.datanode.hostname", false)
-    System.setProperty("HADOOP_USER_NAME", loginAs)
-    new HDFSSink(conf, loginAs, FileSystem.get(conf))
+  override private[jetprobe] def getStorage(config: Map[String,Any]) = {
+    ExpressionParser.parseAll(Seq(Expr(uri),Expr(loginAs)),config) match {
+
+      case Left(exception) => throw exception
+      case Right(parsedMap) =>
+        val conf = new Configuration()
+        conf.set("fs.defaultFS", parsedMap(uri))
+        conf.setBoolean("dfs.client.use.datanode.hostname", false)
+        System.setProperty("HADOOP_USER_NAME", parsedMap(loginAs))
+        new HDFSStorage(conf, parsedMap(loginAs), FileSystem.get(conf))
+
+    }
+
   }
 
 }
